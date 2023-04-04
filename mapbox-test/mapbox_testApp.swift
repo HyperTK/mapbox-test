68bc9cfe77ab8bf5f32840e87f7537e0a946c374
ktakino <kakeru.t0227@gmail.com>
2023-04-04 16:49:03 +0900

    first commit

---
+++ mapbox-test/mapbox_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapbox_testApp.swift
+//  mapbox-test
+//
+//  Created by 滝野駿 on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct mapbox_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
